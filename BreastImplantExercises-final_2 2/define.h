e9be3e34d2087454a721dfd3677bf2bd4fc1d7c5
tinigon <tinigon89@gmail.com>
2013-05-16 20:24:50 +0700

    Update layout ipad

---
+++ BreastImplantExercises-final_2 2/define.h
@@ -0,0 +1,32 @@
+//
+//  define.h
+//  EYoLo
+//
+//  Created by Jacky Nguyen on 3/13/13.
+//  Copyright (c) 2013 teamios. All rights reserved.
+//
+
+#define mainAppDelegate ((AppDelegate *)[[UIApplication sharedApplication] delegate])
+
+#define IMAGESPATH [NSHomeDirectory() stringByAppendingString:@"/Documents/"]
+
+#define HOMEDIRECTORY [[NSBundle mainBundle] resourcePath]
+
+#define DOCUMENTDIRECTORY [NSHomeDirectory() stringByAppendingString:@"/Documents/"]
+
+#define LIBRARY_CATCHES_DIRECTORY [NSHomeDirectory() stringByAppendingString:@"/Library/Caches/"]
+
+#define RADIANS_TO_DEGREES(radians) ((radians) * (180.0 / M_PI))
+#define DEGREES_TO_RADIANS(angle) ((angle) / 180.0 * M_PI)
+
+#ifdef UI_USER_INTERFACE_IDIOM
+#define IS_IPAD() (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#define PORTRAIT_KEYBOARD_HEIGHT 216
+#define LANDSCAPE_KEYBOARD_HEIGHT  162
+#else
+#define IS_IPAD() (false)
+#define PORTRAIT_KEYBOARD_HEIGHT 216
+#define LANDSCAPE_KEYBOARD_HEIGHT  162
+#endif
+
+
